dd575db2eba4c5d2ff11028eb7fc90b77ff8ecde
Lucas Eduardo Silva <99747569+lucaseduardo76@users.noreply.github.com>
2023-03-10 15:19:15 -0300

    Changing variable name

--- src/componentes/interface/button/index.tsx
+++ src/componentes/interface/button/index.tsx
@@ -9,12 +9,12 @@ type Props = {
 
 export const Button = (data: Props) => {
 
-    const cas: string = data.size ? data.size : 'auto';
+    const size: string = data.size ? data.size : 'auto';
 
     return (
         <C.Button  >
             <C.ButtonLink href={data.link} target="_blank">
-                <C.ImgStyled iconSize={cas} src={data.photo} alt="" />
+                <C.ImgStyled iconSize={size} src={data.photo} alt="" />
                 <C.ButtonText>{data.text}</C.ButtonText>
             </C.ButtonLink>
         </C.Button>
