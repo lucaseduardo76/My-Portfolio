e3f89fecdd6f5078d1640e86ef4b752100404489
Lucas Eduardo <lesds2001@gmail.com>
2023-03-18 20:25:46 -0300

    Changing footer

--- src/componentes/pages/rodape/index.tsx
+++ src/componentes/pages/rodape/index.tsx
@@ -6,7 +6,7 @@ export const End = () => {
 
     return(
         <C.Container >
-            <span>Desevolvido por <C.Name href='https://www.linkedin.com/in/lucas-eduardo-silva-071417244/' target='_blank'>Lucas Eduardo</C.Name></span>
+            <span>Desevolvido por <C.Name href='https://www.linkedin.com/in/lucas-eduardo-silva-071417244/' target='_blank'>Lucas Eduardo !</C.Name></span>
         </C.Container>
     )
 }
