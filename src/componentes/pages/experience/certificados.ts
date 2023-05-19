197b55f87fb9b84fdd77469e1c7da5d35db320d9
Lucas Eduardo <lesds2001@gmail.com>
2023-05-19 11:58:19 -0300

    Adding english certificate

--- src/componentes/pages/experience/certificados.ts
+++ src/componentes/pages/experience/certificados.ts
@@ -2,6 +2,7 @@ import typescript from './image/typescript.jpeg'
 import javascript from './image/javascript.jpeg'
 import htmlcss from './image/htmlcss.jpeg'
 import git from './image/git.jpeg'
+import english from './image/english.jpg'
 
 export const CertificationList = () => {
 
@@ -37,6 +38,12 @@ export const CertificationList = () => {
             title: 'Git & Github',
             text: 'Git é um sistema de controle de versão e o GitHub é uma plataforma de hospedagem de código fonte.',
             link: 'https://alunos.b7web.com.br/media/certificates/certificado_4381621.jpg' 
+        },
+        {
+            photo: english,
+            title: 'Inglês',
+            text: 'Conclusão do 4° semestre do curso de inglês da Fisk.',
+            link: 'https://sgfapps.fisk.com.br:156/birt/preview?__report=report/aluno/r_aluno_certificado_1A_model.rptdesign&__format=pdf&__locale=en_US&pCdPessoa=525725&pCdEstagio=137' 
         }
         
     ]
